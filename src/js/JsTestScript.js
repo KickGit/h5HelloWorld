b4eec9a454af8aa7b14e4b8c809bb789b82eb5aa
russell <wang.cugb@gmail.com>
2015-10-15 04:24:14 +0800

    jsp test

---
+++ src/js/JsTestScript.js
@@ -0,0 +1,119 @@
+/**
+ * Created by Kickgit on 2015/10/15.
+ */
+
+
+
+/*
+//Cookie
+function getCookie(c_name){
+
+}
+
+function setCookie(c_name, value, expiredays){
+
+}
+
+function checkCookie(){
+
+}
+
+//11: brower info
+function getBroInfo(){
+    var browser = navigator.appName;
+    var b_version = navigator.appVersion;
+    var version = parseFloat(b_version);
+
+    document.write("bro name:"+ browser)
+    document.write("<br />")
+    document.write("version::"+ version)
+}
+
+
+//10 Test TryCatch
+function trycatch(){
+    var txt = "";
+    try{
+        adddlert("hello");
+    }catch(err){
+        txt +="err desc: "+err.description;
+        if(!confirm(txt)){
+            document.location.href="/JsTest.html";
+        }
+    }
+
+}
+
+
+//9 Test Array
+function forArray(){
+    var x;
+    var mycars = new Array();
+    mycars[0]="bench"
+    mycars[1]="binli"
+    mycars[2]="baoma"
+    for(x in mycars){
+        document.write(mycars[x]+"<br/>")
+    }
+}
+
+//8 for Test
+function forTest(){
+    for(i= 0; i< 5; i++){
+        document.write(i);
+    }
+}
+
+//7  func Test two param
+function myfunction_two(a,b){
+    return a*b;
+}
+
+//6 func Test one param
+function myfunction(txt){
+    return txt;
+}
+//5 ÌáÊ¾¿ò
+function disp_prompt(){
+    var name= prompt("Your name","Bill Gates");
+    if(name != null && name != ''){
+        document.write("Hello "+name+" !")
+    }
+}
+
+
+//4 test confirm dialog
+function show_confirm(){
+    var con = confirm("Submit the value?");
+    if(con == true){
+        document.write("Submit Success");
+    }else{
+        document.write("Cancel");
+    }
+}
+
+//3 Test form action
+ function disp_alert(){
+ alert('Submit Success!'+'\n'+'Line 2');
+ }
+*/
+
+/*
+ //2 Test Random
+ var ran = Math.random();
+ document.write("Random is: "+ran);*/
+
+// 1 Test Time
+/*var d = new Date();
+var time = d.getHours();
+if(time < 10 && time>5){
+    document.write('Good Morning'+time);
+}else if(time>0 && time <=5){
+    document.write('You need sleep');
+}else{
+    document.write('How old are u');
+}*/
+
+
+
+
